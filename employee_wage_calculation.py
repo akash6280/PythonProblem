6667ce9a21fcbbd4a96c2318f9c692f9f3eea59f
Akash Singh <akashsingh6280@gmail.com>
2021-11-01 02:12:48 +0530

    [akash]REFACTORED: Calculate employee wage for multiple companies

--- employee_wage_calculation.py
+++ employee_wage_calculation.py
@@ -3,22 +3,17 @@ import random
 
 
 class EmployeeWageCalculation:
+    """ class that represents employee wage calculation"""
     IS_ABSENT = 0
     IS_FULL_TIME = 1
     IS_PART_TIME = 2
 
-    EMPLOYEE_WAGE_PER_HOUR = 20
-    NUMBER_OF_WORKING_DAYS = 20
-    MAXIMUM_HOURS_IN_MONTH = 100
-    employee_hour = 0
-    total_employee_wage = 0
-
-    def compute_employee_wage(self):
+    def compute_employee_wage(self, company_name, employee_wage_per_hour, number_0f_working_days,
+                              maximum_hour_in_month):
         """ method to calculate employee total wage"""
         total_working_days = 0
         total_employee_hours = 0
-
-        while total_working_days < self.NUMBER_OF_WORKING_DAYS and total_employee_hours <= self.MAXIMUM_HOURS_IN_MONTH:
+        while total_working_days < number_0f_working_days and total_employee_hours <= maximum_hour_in_month:
             employee_check = math.floor(random.random() * 10) % 3
             if employee_check == self.IS_FULL_TIME:
                 employee_hour = 8
@@ -28,9 +23,11 @@ class EmployeeWageCalculation:
                 employee_hour = 0
             total_employee_hours = total_employee_hours + employee_hour
             total_working_days = total_working_days + 1
-        self.total_employee_wage = total_employee_hours * self.EMPLOYEE_WAGE_PER_HOUR
+        total_employee_wage = total_employee_hours * employee_wage_per_hour
+        print(company_name)
+        print("Total Employee Wage :" + str(total_employee_wage))
 
 
 employee = EmployeeWageCalculation()
-employee.compute_employee_wage()
-print(" Total Employee Wage :" + str(employee.total_employee_wage))
+employee.compute_employee_wage("TCS", 20, 20, 100)
+employee.compute_employee_wage("INFOSYS", 30, 40, 90)
