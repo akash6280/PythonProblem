ec8eab3b20020cb4c772cf6b3d313478231fd624
Akash Singh <akashsingh6280@gmail.com>
2021-11-01 00:46:12 +0530

    [akash]MODIFIED: Calculate wages till working days<=20 or working hours<=100

--- employee_wage_calculation.py
+++ employee_wage_calculation.py
@@ -7,12 +7,14 @@ IS_PART_TIME = 2
 
 EMPLOYEE_WAGE_PER_HOUR = 20
 NUMBER_OF_WORKING_DAYS = 20
+MAXIMUM_HOURS_IN_MONTH = 100
 employee_hour = 0
 total_employee_wage = 0
+total_working_days = 0
+total_employee_hours = 0
 
-
-for i in range(NUMBER_OF_WORKING_DAYS):
-    print(i)
+while total_working_days < NUMBER_OF_WORKING_DAYS and total_employee_hours <= MAXIMUM_HOURS_IN_MONTH:
+    
     employee_check = math.floor(random.random() * 10) % 3
     if employee_check == IS_FULL_TIME:
         employee_hour = 8
@@ -20,7 +22,8 @@ for i in range(NUMBER_OF_WORKING_DAYS):
         employee_hour = 4
     else:
         employee_hour = 0
-    employee_wage = employee_hour * EMPLOYEE_WAGE_PER_HOUR
-    total_employee_wage = total_employee_wage + employee_wage
+    total_employee_hours += employee_hour
+    total_working_days = total_working_days + 1
 
+total_employee_wage = total_employee_hours * EMPLOYEE_WAGE_PER_HOUR
 print(" Total Employee Wage :" + str(total_employee_wage))
