934b4ae53f136053e5882539f5cd7affea63feed
Akash Singh <akashsingh6280@gmail.com>
2021-10-31 23:06:24 +0530

    [akash] ADDED: program to demonstrate different data type

---
+++ data_types.py
@@ -0,0 +1,9 @@
+int_variable = 10
+float_variable = 11.2
+string_variable = "Python"
+boolean_variable = True
+
+print(int_variable)
+print(float_variable)
+print(string_variable)
+print(boolean_variable)
