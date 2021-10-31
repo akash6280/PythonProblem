db2b6445746fe87ba595954de752109e8133b5ee
Akash Singh <akashsingh6280@gmail.com>
2021-11-01 00:17:20 +0530

    [akash]MODIFIED: To calculate part time wage

--- employee_wage_calculation.py
+++ employee_wage_calculation.py
@@ -2,17 +2,19 @@ import math
 import random
 
 IS_ABSENT = 0
-IS_PRESENT = 1
+IS_FULL_TIME = 1
+IS_PART_TIME = 2
+
 EMPLOYEE_WAGE_PER_HOUR = 20
 employee_hour = 0
-employee_check = math.floor(random.random() * 10) % 2
+employee_check = math.floor(random.random() * 10) % 3
 
-if employee_check == IS_PRESENT:
-    print("employee present")
+if employee_check == IS_FULL_TIME:
     employee_hour = 8
+elif employee_check == IS_PART_TIME:
+    employee_hour = 4
 else:
-    print("employee absent")
     employee_hour = 0
 
 employee_wage = employee_hour * EMPLOYEE_WAGE_PER_HOUR
-print("Employee Wage :"+str(employee_wage))
+print("Employee Wage :" + str(employee_wage))
