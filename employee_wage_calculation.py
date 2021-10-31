e16ba42633ab315180276003dec2f27d99507e02
Akash Singh <akashsingh6280@gmail.com>
2021-11-01 00:11:43 +0530

    [akash]ADDED: To calculate employee daily wage

--- employee_wage_calculation.py
+++ employee_wage_calculation.py
@@ -1,11 +1,18 @@
 import math
 import random
+
 IS_ABSENT = 0
 IS_PRESENT = 1
-
+EMPLOYEE_WAGE_PER_HOUR = 20
+employee_hour = 0
 employee_check = math.floor(random.random() * 10) % 2
 
 if employee_check == IS_PRESENT:
     print("employee present")
+    employee_hour = 8
 else:
     print("employee absent")
+    employee_hour = 0
+
+employee_wage = employee_hour * EMPLOYEE_WAGE_PER_HOUR
+print("Employee Wage :"+str(employee_wage))
