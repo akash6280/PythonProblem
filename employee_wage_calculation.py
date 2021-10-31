fc6e702cdbec63abfe9187cca242b0953a70eafd
Akash Singh <akashsingh6280@gmail.com>
2021-11-01 02:40:00 +0530

    [akash]REFACTORED: Calculated employee wage using instance variable

--- employee_wage_calculation.py
+++ employee_wage_calculation.py
@@ -8,12 +8,19 @@ class EmployeeWageCalculation:
     IS_FULL_TIME = 1
     IS_PART_TIME = 2
 
-    def compute_employee_wage(self, company_name, employee_wage_per_hour, number_0f_working_days,
-                              maximum_hour_in_month):
+    def __init__(self, company_name, employee_wage_per_hour, number_of_working_days,
+                 maximum_hour_in_month):
+        """ constructor to initialize instance variable"""
+        self.company_name = company_name;
+        self.employee_wage_per_hour = employee_wage_per_hour
+        self.number_of_working_days = number_of_working_days
+        self.maximum_hour_in_month = maximum_hour_in_month
+
+    def compute_employee_wage(self):
         """ method to calculate employee total wage"""
         total_working_days = 0
         total_employee_hours = 0
-        while total_working_days < number_0f_working_days and total_employee_hours <= maximum_hour_in_month:
+        while total_working_days < self.number_of_working_days and total_employee_hours <= self.maximum_hour_in_month:
             employee_check = math.floor(random.random() * 10) % 3
             if employee_check == self.IS_FULL_TIME:
                 employee_hour = 8
@@ -23,11 +30,12 @@ class EmployeeWageCalculation:
                 employee_hour = 0
             total_employee_hours = total_employee_hours + employee_hour
             total_working_days = total_working_days + 1
-        total_employee_wage = total_employee_hours * employee_wage_per_hour
-        print(company_name)
+        total_employee_wage = total_employee_hours * self.employee_wage_per_hour
+        print(self.company_name)
         print("Total Employee Wage :" + str(total_employee_wage))
 
 
-employee = EmployeeWageCalculation()
-employee.compute_employee_wage("TCS", 20, 20, 100)
-employee.compute_employee_wage("INFOSYS", 30, 40, 90)
+employee_tcs = EmployeeWageCalculation("TCS", 20, 20, 100)
+employee_tcs.compute_employee_wage()
+employee_infosys = EmployeeWageCalculation("Infosys", 15, 18, 80)
+employee_infosys.compute_employee_wage()
