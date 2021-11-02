8b40db42f02334fb19ea0aa8426de29543020e91
Akash Singh <akashsingh6280@gmail.com>
2021-11-02 20:37:09 +0530

    [akash] ADDED: rolling die to get a number

--- snake_and_ladder.py
+++ snake_and_ladder.py
@@ -1,2 +1,6 @@
+import math
+import random
+
 start_position = 0
-print(f"start position {start_position}")
+number_on_die = math.floor(random.random() * 10) % 6 + 1
+print(f"Number on die {number_on_die}")
