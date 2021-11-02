6ff86115de898978420e94ab94cca17179b557e1
Akash Singh <akashsingh6280@gmail.com>
2021-11-02 21:08:03 +0530

    [akash] REFACTORED: report position of dice and no of plays

--- snake_and_ladder.py
+++ snake_and_ladder.py
@@ -6,10 +6,11 @@ LADDER = 1
 SNAKE = 2
 START_POSITION = 0
 current_position = START_POSITION
+no_of_plays =0
 while current_position < 100:
     number_on_die = math.floor(random.random() * 10) % 6 + 1
     option = math.floor(random.random() * 10) % 3
-
+    no_of_plays += 1
     if option == NO_PLAY:
         pass
     elif option == LADDER:
@@ -23,6 +24,8 @@ while current_position < 100:
             current_position = 0
     if current_position == 100:
         break
+    print(current_position)
 
 print("Player wins")
+print(f"no of plays {no_of_plays}")
 print(f"position at end {current_position}")
