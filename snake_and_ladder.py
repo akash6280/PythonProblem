013a5f9e4a6bf6593e70bab8bdc04b873b152832
Akash Singh <akashsingh6280@gmail.com>
2021-11-02 20:48:59 +0530

    [akash] ADDED: checking option for play

--- snake_and_ladder.py
+++ snake_and_ladder.py
@@ -1,6 +1,20 @@
 import math
 import random
 
-start_position = 0
+NO_PLAY = 0
+LADDER = 1
+SNAKE = 2
+START_POSITION = 0
+current_position = START_POSITION
 number_on_die = math.floor(random.random() * 10) % 6 + 1
 print(f"Number on die {number_on_die}")
+
+option = math.floor(random.random() * 10) % 3
+
+if option == NO_PLAY:
+    pass
+elif option == LADDER:
+    current_position += number_on_die
+else:
+    current_position -= number_on_die
+print(f"current position {current_position}")
