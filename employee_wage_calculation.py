b0480d293cd29a36aa9a4be805aeb9cf52fea1dc
Akash Singh <akashsingh6280@gmail.com>
2021-11-02 19:41:03 +0530

    [akash] REFACTORED: calculate wage using class object list

--- employee_wage_calculation.py
+++ employee_wage_calculation.py
@@ -1,4 +1,5 @@
 import math
+from company_employee_wage import CompanyEmployeeWage
 import random
 
 
@@ -7,20 +8,25 @@ class EmployeeWageCalculation:
     IS_ABSENT = 0
     IS_FULL_TIME = 1
     IS_PART_TIME = 2
+    company_employee_wage_list = []
 
-    def __init__(self, company_name, employee_wage_per_hour, number_of_working_days,
-                 maximum_hour_in_month):
-        """ constructor to initialize instance variable"""
-        self.company_name = company_name;
-        self.employee_wage_per_hour = employee_wage_per_hour
-        self.number_of_working_days = number_of_working_days
-        self.maximum_hour_in_month = maximum_hour_in_month
+    def add_company_employee_wage(self, company_name, wage_per_hour, no_of_days, max_hours_per_month):
+        self.company_employee_wage_list.append(
+            CompanyEmployeeWage(company_name, wage_per_hour, no_of_days, max_hours_per_month))
 
-    def compute_employee_wage(self):
+    def calculate_company_employee_wage(self):
+        for i in range(len(self.company_employee_wage_list)):
+            self.company_employee_wage_list[i].set_total_employee_wage(
+                self.calculate_employee_wage(self.company_employee_wage_list[i]))
+            print(self.company_employee_wage_list[i])
+
+    def calculate_employee_wage(self, employee):
         """ method to calculate employee total wage"""
-        total_working_days = 0
         total_employee_hours = 0
-        while total_working_days < self.number_of_working_days and total_employee_hours <= self.maximum_hour_in_month:
+        total_working_days = 0
+        while total_employee_hours < employee.maximum_hour_in_month and \
+                total_working_days < employee.number_of_working_days:
+
             employee_check = math.floor(random.random() * 10) % 3
             if employee_check == self.IS_FULL_TIME:
                 employee_hour = 8
@@ -30,12 +36,14 @@ class EmployeeWageCalculation:
                 employee_hour = 0
             total_employee_hours = total_employee_hours + employee_hour
             total_working_days = total_working_days + 1
-        total_employee_wage = total_employee_hours * self.employee_wage_per_hour
-        print(self.company_name)
-        print("Total Employee Wage :" + str(total_employee_wage))
+        print(employee.company_name)
+        print("Total Employee Wage :{0}".format(
+            str(total_employee_hours * employee.employee_wage_per_hour)))
+        return total_employee_hours * employee.employee_wage_per_hour
 
 
-employee_tcs = EmployeeWageCalculation("TCS", 20, 20, 100)
-employee_tcs.compute_employee_wage()
-employee_infosys = EmployeeWageCalculation("Infosys", 15, 18, 80)
-employee_infosys.compute_employee_wage()
+employee = EmployeeWageCalculation()
+employee.add_company_employee_wage("TCS", 20, 6, 10)
+employee.add_company_employee_wage("WIPRO", 10, 5, 20)
+employee.add_company_employee_wage("INFOSYS", 10, 5, 20)
+employee.calculate_company_employee_wage()
