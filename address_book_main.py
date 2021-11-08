21151186d8dcbac401d5822770020d81599b9fa3
Akash Singh <akashsingh6280@gmail.com>
2021-11-08 22:57:28 +0530

    [akash] ADDED: contact detail added to list

---
+++ address_book_main.py
@@ -0,0 +1,6 @@
+from contact import Contact
+
+address_book = []
+contact_person = Contact("akash", "singh", "hsr", "bangalore", "karnataka", "221002", "8090433565", "abc@gmail.com")
+address_book.append(contact_person)
+print(contact_person)
