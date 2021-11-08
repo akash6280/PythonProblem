de6f35dd3e4448b4c934a40e8cab7c3df065b1ec
Akash Singh <akashsingh6280@gmail.com>
2021-11-09 00:03:23 +0530

    [akash] ADDED: method to delete contact detail

--- address_book_main.py
+++ address_book_main.py
@@ -11,6 +11,14 @@ def edit_contact(new_detail, updated_contact):
             address_book[i] = updated_contact
 
 
+def delete_contact(contact_to_delete):
+    """ method to delete contact using first name"""
+    for i in range(len(address_book)):
+        contact = address_book[i]
+        if contact.first_name == contact_to_delete:
+            address_book.remove(contact)
+
+
 def print_address_book():
     """ method to print address book list"""
     for i in range(len(address_book)):
@@ -20,10 +28,14 @@ def print_address_book():
 
 contact_person = Contact("akash", "singh", "hsr", "bangalore", "karnataka", "221002", "8090433565", "abc@gmail.com")
 address_book.append(contact_person)
-print(contact_person)
+print_address_book()
 
 contact_to_edit = "8090433565"
 updated_contact_person = Contact("akash", "singh", "hsr", "bangalore", "karnataka", "221002", "8090433565", "xyz"
                                                                                                             "@gmail.com")
 edit_contact(contact_to_edit, updated_contact_person)
+print_address_book()
+
+contact_to_delete = "akash"
+delete_contact(contact_to_delete)
 print_address_book()
