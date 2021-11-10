2564caf554ab1228d83187f4c9d51803b2856ea7
Akash Singh <akashsingh6280@gmail.com>
2021-11-10 15:18:06 +0530

    [akash] REFACTORED : used class for managing operations on address book

--- address_book_main.py
+++ address_book_main.py
@@ -1,66 +1,132 @@
 from contact import Contact
-
-address_book = []
-
-
-def edit_contact(new_detail, updated_contact):
-    """ method to edit contact detail using phone number"""
-    for i in range(len(address_book)):
-        contact = address_book[i]
-        if contact.phone == new_detail:
-            address_book[i] = updated_contact
+import json
 
 
-def delete_contact(contact_to_delete):
-    """ method to delete contact using first name"""
-    for i in range(len(address_book)):
-        contact = address_book[i]
-        if contact.first_name == contact_to_delete:
-            address_book.remove(contact)
+class AddressBook:
+    """class that manages operations on addressbook"""
 
+    def __init__(self):
+        self.address_book = []
+        self.read_data_from_json_file()
 
-def search_person_by_state(state):
-    """ method to search person by state in address book"""
-    for i in range(len(address_book)):
-        contact = address_book[i]
-        if contact.state == state:
+    def add_contact(self, contact):
+        """method to add contact to list """
+        self.address_book.append(contact)
+        self.write_data_to_json_file()
+
+    def edit_contact(self, new_detail, updated_contact):
+        """ method to edit contact detail using phone number"""
+        print(len(self.address_book))
+        for i in range(len(self.address_book)):
+            contact = self.address_book[i]
+            if contact.phone == new_detail:
+                self.address_book[i] = updated_contact
+        self.write_data_to_json_file()
+
+    def delete_contact(self, contact_to_delete):
+        """ method to delete contact using first name"""
+        for each_a in self.address_book:
+            contact = each_a
+            if contact.first_name == contact_to_delete:
+                self.address_book.remove(contact)
+        self.write_data_to_json_file()
+
+    def search_person_by_state(self, state):
+        """ method to search person by state in address book"""
+        for i in range(len(self.address_book)):
+            contact = self.address_book[i]
+            if contact.state == state:
+                print(contact)
+
+    def search_person_by_city(self, city):
+        """ method to search person by city in address book"""
+        for i in range(len(self.address_book)):
+            contact = self.address_book[i]
+            if contact.city == city:
+                print(contact)
+
+    def write_data_to_json_file(self):
+        """ method to write data to json file"""
+        address_books = []
+        for i in range(len(self.address_book)):
+            contact = self.address_book[i]
+            address_books.append(contact.json_object())
+        with open("address.json", 'w') as output_file:
+            json.dump(address_books, output_file)
+
+    def read_data_from_json_file(self):
+        """method to read data from json file"""
+        with open("address.json") as json_file:
+            data = json.load(json_file)
+        for contact in data:
+            first_name = contact["first_name"]
+            last_name = contact["last_name"]
+            address = contact["address"]
+            city = contact["city"]
+            state = contact["state"]
+            zip_code = contact["zip_code"]
+            phone = contact["phone"]
+            email = contact["email"]
+            self.address_book.append(Contact(first_name, last_name, address, city
+                                             , state, zip_code, phone, email))
+
+    def print_address_book(self):
+        """ method to print address book list"""
+        for i in range(len(self.address_book)):
+            contact = self.address_book[i]
             print(contact)
 
 
-def search_person_by_city(city):
-    """ method to search person by city in address book"""
-    for i in range(len(address_book)):
-        contact = address_book[i]
-        if contact.city == city:
-            print(contact)
+if __name__ == "__main__":
+    address_book_object = AddressBook()
+    while True:
+        print(" 1 for add contact \n 2 for edit contact \n 3 for delete contact \n 4 search person by city \n 5 search "
+              "person by state \n 6 map city with person \n 7 map state with person \n 8 to exit")
+        option = int(input("enter choice "))
+        if option == 1:
+            first_name = input("enter first name \n")
+            last_name = input("enter last name \n ")
+            address = input("enter address \n")
+            city = input("enter city \n ")
+            state = input("enter state \n ")
+            zip_code = input("enter zip code \n")
+            phone = input("enter phone number \n")
+            email = input("enter email \n")
+            address_book_object.add_contact(
+                Contact(first_name, last_name, address, city, state, zip_code, phone, email))
+            address_book_object.print_address_book()
 
+        elif option == 2:
+            phone = input("enter phone number \n")
+            first_name = input("enter first name \n")
+            last_name = input("enter last name \n")
+            address = input("enter address \n")
+            city = input("enter city \n")
+            state = input("enter state \n")
+            zip_code = input("enter zip code \n")
+            email = input("enter email \n")
+            updated_contact_person = Contact(first_name, last_name, address, city, state, zip_code, phone, email)
+            address_book_object.edit_contact(phone, updated_contact_person)
+            address_book_object.print_address_book()
 
-def print_address_book():
-    """ method to print address book list"""
-    for i in range(len(address_book)):
-        contact = address_book[i]
-        print(contact)
+        elif option == 3:
+            name = input("enter contact name \n ")
+            address_book_object.delete_contact(name)
+            address_book_object.print_address_book()
 
+        elif option == 4:
+            city = input("enter city name \n")
+            address_book_object.search_person_by_city(city)
 
-contact_person = Contact("akash", "singh", "hsr", "bangalore", "karnataka", "221002", "8090433565", "abc@gmail.com")
-address_book.append(contact_person)
-print_address_book()
+        elif option == 5:
+            state = input("enter state name \n")
+            address_book_object.search_person_by_state(state)
 
-contact_to_edit = "8090433565"
-updated_contact_person = Contact("akash", "singh", "hsr", "bangalore", "karnataka", "221002", "8090433565", "xyz"
-                                                                                                            "@gmail.com")
-edit_contact(contact_to_edit, updated_contact_person)
-print_address_book()
+        elif option == 6:
+            address_book_object.map_city_with_person()
 
-contact_to_delete = "akash"
-delete_contact(contact_to_delete)
-print_address_book()
+        elif option == 7:
+            address_book_object.map_state_with_person()
 
-address_book.append(contact_person)
-contact_person = Contact("agam", "singh", "cantt", "varanasi", "uttar pradesh", "221002", "9740142826",
-                         "agam@gmail.com")
-address_book.append(contact_person)
-
-search_person_by_state("uttar pradesh")
-
-search_person_by_city("bangalore")
+        else:
+            exit()
