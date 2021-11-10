282d88783bb853bf8dea224bd8999ec472968dea
Akash Singh <akashsingh6280@gmail.com>
2021-11-10 20:11:58 +0530

    [akash] ADDED: method to verify email id using regex

--- user_registration.py
+++ user_registration.py
@@ -23,12 +23,24 @@ class UserRegistration():
         else:
             print("last name is not valid")
 
+    @staticmethod
+    def validate_email(email_to_be_validated):
+        pattern = "^[a-zA-Z][a-zA-Z0-9]*[a-zA-Z0-9]([+-_.][a-zA-Z]*)?@[a-zA-Z0-9]*[.][a-z]{2,4}([.][a-zA-z]{2})?$"
+        result = re.match(pattern, email_to_be_validated)
+        if result:
+            print("email is valid")
+        else:
+            print("email is not valid")
+
 
 if __name__ == "__main__":
-    first_name = input("enter name to be checked ")
+    first_name = input("enter first name to be checked ")
     UserRegistration.validate_first_name(first_name)
 
-    last_name = input("enter name to be checked ")
+    last_name = input("enter last name to be checked ")
     UserRegistration.validate_last_name(last_name)
+
+    email_id = input("enter email to be checked ")
+    UserRegistration.validate_email(email_id)
 
 
