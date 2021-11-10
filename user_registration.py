db2e9d8cc91a8baea38f7285c8a7b5852c7c3ed3
Akash Singh <akashsingh6280@gmail.com>
2021-11-10 19:52:53 +0530

    [akash] ADDED: method to verify last name using regex

--- user_registration.py
+++ user_registration.py
@@ -10,11 +10,25 @@ class UserRegistration():
         pattern = "^[A-Z][A-Za-z]{2,}$"
         result = re.match(pattern, name_to_be_validated)
         if result:
-            print("name is valid")
+            print("first name is valid")
         else:
-            print("name is not valid")
+            print("first name is not valid")
+
+    @staticmethod
+    def validate_last_name(name_to_be_validated):
+        pattern = "^[A-Z][A-Za-z]{2,}$"
+        result = re.match(pattern, name_to_be_validated)
+        if result:
+            print("last name is valid")
+        else:
+            print("last name is not valid")
 
 
 if __name__ == "__main__":
     first_name = input("enter name to be checked ")
     UserRegistration.validate_first_name(first_name)
+
+    last_name = input("enter name to be checked ")
+    UserRegistration.validate_last_name(last_name)
+
+
