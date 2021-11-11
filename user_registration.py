bf80f6d9e5eafd6eca60f0d1c3066242cbc35efa
Akash Singh <akashsingh6280@gmail.com>
2021-11-11 22:15:19 +0530

    [akash] REFACTORED : used custom exception class to handle exceptions

--- user_registration.py
+++ user_registration.py
@@ -1,54 +1,86 @@
 import re
 
+from user_registration_exception import UserRegistrationException
 
-class UserRegistration():
+
+class UserRegistration:
     def __init__(self):
         pass
 
     @staticmethod
     def validate_first_name(name_to_be_validated):
         pattern = "^[A-Z][A-Za-z]{2,}$"
-        result = re.match(pattern, name_to_be_validated)
-        if result:
-            print("first name is valid")
-        else:
-            print("first name is not valid")
+        try:
+            if name_to_be_validated is None:
+                raise UserRegistrationException("None String entered")
+            elif name_to_be_validated is "":
+                raise UserRegistrationException("Empty String")
+            elif re.match(pattern, name_to_be_validated):
+                print("first name is valid")
+            else:
+                print("first name is not valid")
+        except UserRegistrationException as e:
+            print(e)
 
     @staticmethod
     def validate_last_name(name_to_be_validated):
         pattern = "^[A-Z][A-Za-z]{2,}$"
-        result = re.match(pattern, name_to_be_validated)
-        if result:
-            print("last name is valid")
-        else:
-            print("last name is not valid")
+        try:
+            if name_to_be_validated is None:
+                raise UserRegistrationException("None String entered")
+            elif name_to_be_validated is "":
+                raise UserRegistrationException("Empty String")
+            elif re.match(pattern, name_to_be_validated):
+                print("last name is valid")
+            else:
+                print("last name is not valid")
+        except UserRegistrationException as e:
+            print(e)
 
     @staticmethod
     def validate_email(email_to_be_validated):
         pattern = "^[a-zA-Z][a-zA-Z0-9]*[a-zA-Z0-9]([+-_.][a-zA-Z]*)?@[a-zA-Z0-9]*[.][a-z]{2,4}([.][a-zA-z]{2})?$"
-        result = re.match(pattern, email_to_be_validated)
-        if result:
-            print("email is valid")
-        else:
-            print("email is not valid")
+        try:
+            if email_to_be_validated is None:
+                raise UserRegistrationException("None String entered")
+            elif email_to_be_validated is "":
+                raise UserRegistrationException("Empty String")
+            elif re.match(pattern, email_to_be_validated):
+                print("email is valid")
+            else:
+                print("email is not valid")
+        except UserRegistrationException as e:
+            print(e)
 
     @staticmethod
     def validate_phone(phone_to_be_validated):
         pattern = "^[0-9]{2}[ ][1-9][0-9]{9}$"
-        result = re.match(pattern, phone_to_be_validated)
-        if result:
-            print("phone number is valid")
-        else:
-            print("phone number is not valid")
+        try:
+            if phone_to_be_validated is None:
+                raise UserRegistrationException("None String entered")
+            elif phone_to_be_validated is "":
+                raise UserRegistrationException("Empty String")
+            elif re.match(pattern, phone_to_be_validated):
+                print("phone is valid")
+            else:
+                print("phone is not valid")
+        except UserRegistrationException as e:
+            print(e)
 
     @staticmethod
     def validate_password(password_to_be_validated):
         pattern = "(?=.*[A-Z])(?=.*?[0-9])(?=.*[#?!@$%^&*-])(?=.*[#?!@$%^&*-]).{8,}$"
-        result = re.match(pattern, password_to_be_validated)
-        if result:
-            print("password is valid")
-        else:
-            print("password is not valid")
+        try:
+            if password_to_be_validated is None:
+                raise UserRegistrationException("None String entered")
+            elif password_to_be_validated is "":
+                raise UserRegistrationException("Empty String")
+            elif re.match(pattern, password_to_be_validated):
+                print("password is valid")
+            else:
+                print("password is not valid")
+        except UserRegistrationException as e:
+            print(e)
 
 
 if __name__ == "__main__":
@@ -66,5 +98,3 @@ if __name__ == "__main__":
 
     password = input("enter password to be checked ")
     UserRegistration.validate_password(password)
-
-
