bc7b0618719c4644812d1cdf0611eb36067cd270
Akash Singh <akashsingh6280@gmail.com>
2021-11-16 00:40:01 +0530

    [akash] ADDED: program to find the real and imaginary parts of an array of complex numbers

---
+++ complex_numbers.py
@@ -0,0 +1,7 @@
+
+import numpy as np
+
+complex_numbers = np.array([1.00000000+0.j, 0.70710678+0.70710678j])
+
+print("Real part of the array: ", complex_numbers.real)
+print("Imaginary part of the array: ", complex_numbers.imag)
