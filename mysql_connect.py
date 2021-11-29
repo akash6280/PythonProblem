d99d7c760bd09ef966675e3324f89047175c1588
Akash Singh <akashsingh6280@gmail.com>
2021-11-30 00:31:46 +0530

    [akash] REFACTORED: description of method added

--- mysql_connect.py
+++ mysql_connect.py
@@ -3,6 +3,13 @@ from mysql.connector import connect, Error
 
 
 def get_connection(USERNAME, PASSWORD, DATABASE):
+    """
+       function to create and return the connection to database
+       :param USERNAME:  username to use in the database
+       :param PASSWORD: password for the username
+       :param DATABASE: database to be used
+       :return: connection object
+       """
     try:
         connection = connect(
             host="localhost",
