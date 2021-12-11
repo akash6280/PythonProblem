cf358c51448e6adfd5d96c8273261efae849b7e9
Akash Singh <akashsingh6280@gmail.com>
2021-12-11 18:47:14 +0530

    [akash] ADDED: method to return json object

---
+++ address_book_system/contact.py
@@ -0,0 +1,30 @@
+class Contact:
+    """ class that represent person details """
+
+    def __init__(self, first_name, last_name, address, city, state, zip_code, phone, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+        self.phone = phone
+        self.email = email
+
+    def __str__(self):
+        return "first_name: " + self.first_name + "\nlast_name: " + self.last_name + "\naddress: " + self.address \
+               + "\ncity: " + self.city + "\nstate: " + self.state + "\nzip_code: " + self.zip_code + "\nphone: " \
+               + self.phone + "\nemail: " + self.email
+
+    def json_object(self):
+        """ method that return json object"""
+        return {
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "address": self.address,
+            "city": self.city,
+            "state": self.state,
+            "zip_code": self.zip_code,
+            "phone": self.phone,
+            "email": self.email
+        }
