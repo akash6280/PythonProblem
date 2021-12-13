5c68b272a42972058437f0d4f5f344802e5aa364
Akash Singh <akashsingh6280@gmail.com>
2021-12-14 01:31:13 +0530

    [akash] ADDED : test cases for failing scenarios

--- address_book_system/address_book_test.py
+++ address_book_system/address_book_test.py
@@ -12,41 +12,88 @@ def address_book():
     return address_book
 
 
-def test_add_contact(address_book):
+def test_add_contact_with_correct_count(address_book):
     assert len(address_book.address_book) == 1
 
 
-def test_edit_contact(address_book):
-    new_detail = "xyz@gmail.com"
+def test_edit_contact_that_exist(address_book):
+    unique_detail = "9740142826"
     contact_to_update = Contact("agam", "singh", "cantt", "varanasi", "uttar pradesh", "221002", "9740142826",
-                                "agam@gmail.com")
-    address_book.edit_contact(new_detail, contact_to_update)
-    assert len(address_book.address_book) == 1
+                                "xyz@gmail.com")
+    contact = address_book.edit_contact(unique_detail, contact_to_update)
+    assert contact.phone == contact_to_update.phone
 
 
-def test_delete_contact(address_book):
+def test_delete_contact_with_correct_count(address_book):
     contact_to_delete = "agam"
     address_book.delete_contact(contact_to_delete)
     assert len(address_book.address_book) == 0
 
 
-def test_search_person_by_city(address_book):
+def test_search_person_by_city_with_correct_city(address_book):
     city_to_be_searched = "varanasi"
-    address_book.search_person_by_city(city_to_be_searched)
-    assert len(address_book.address_book) == 1
+    result = address_book.search_person_by_city(city_to_be_searched)
+    assert result == True
 
 
-def test_search_person_by_state(address_book):
+def test_search_person_by_state_with_correct_state(address_book):
     state_to_be_searched = "uttar pradesh"
-    address_book.search_person_by_state(state_to_be_searched)
-    assert len(address_book.address_book) == 1
+    result = address_book.search_person_by_state(state_to_be_searched)
+    assert result == True
 
 
-def test_map_state_with_person(address_book):
+def test_map_state_with_person_with_existing_state(address_book):
     state_map = address_book.map_state_with_person()
     assert len(state_map["uttar pradesh"]) == 1
 
 
-def test_map_city_with_person(address_book):
+def test_map_city_with_person_with_existing_city(address_book):
     city_map = address_book.map_city_with_person()
     assert len(city_map["varanasi"]) == 1
+
+
+@pytest.mark.xfail
+def test_add_contact_return_incorrect_count(address_book):
+    assert len(address_book.address_book) == 0
+
+
+@pytest.mark.xfail
+def test_edit_contact_that_doesnot_exist(address_book):
+    unique_detail = "123456789"
+    contact_to_update = Contact("agam", "singh", "cantt", "varanasi", "uttar pradesh", "221002", "123456789",
+                                "abc@gmail.com")
+    contact = address_book.edit_contact(unique_detail, contact_to_update)
+    assert contact.phone == contact_to_update.phone
+
+
+@pytest.mark.xfail
+def test_delete_contact_with_incorrect_count(address_book):
+    contact_to_delete = "adam"
+    address_book.delete_contact(contact_to_delete)
+    assert len(address_book.address_book) == 0
+
+
+@pytest.mark.xfail
+def test_search_person_by_city_with_incorrect_city(address_book):
+    city_to_be_searched = "chennai"
+    result = address_book.search_person_by_city(city_to_be_searched)
+    assert result == True
+
+
+@pytest.mark.xfail
+def test_search_person_by_state_with_incorrect_state(address_book):
+    state_to_be_searched = "karnataka"
+    result = address_book.search_person_by_state(state_to_be_searched)
+    assert result == True
+
+
+@pytest.mark.xfail
+def test_map_state_with_person_with_state_not_existing(address_book):
+    state_map = address_book.map_state_with_person()
+    assert len(state_map["MP"]) == 1
+
+
+@pytest.mark.xfail
+def test_map_city_with_person_with_city_not_existing(address_book):
+    city_map = address_book.map_city_with_person()
+    assert len(city_map["bhopal"]) == 1
