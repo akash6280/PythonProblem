4e8c557918693513ba0bb282bceff77c3334fe72
Akash Singh <akashsingh6280@gmail.com>
2021-12-15 01:59:54 +0530

    [akash] ADDED: test cases for different scenarios

--- address_book_system/address_book_test.py
+++ address_book_system/address_book_test.py
@@ -1,10 +1,16 @@
 import pytest
+
+from address_book_exception import AddressBookException
 from contact import Contact
 from address_book_main import AddressBook
 
 
 @pytest.fixture()
 def address_book():
+    """
+        function to create contact object and add it  to addressbook
+        :return: oject
+    """
     contact_person = Contact("agam", "singh", "cantt", "varanasi", "uttar pradesh", "221002", "9740142826",
                              "agam@gmail.com")
     address_book = AddressBook()
@@ -12,11 +18,21 @@ def address_book():
     return address_book
 
 
-def test_add_contact_with_correct_count(address_book):
+def test_add_contact_with_correct_details(address_book):
+    """
+       to test add contact with correct input
+       :param - address_book:
+       :return: None
+    """
     assert len(address_book.address_book) == 1
 
 
 def test_edit_contact_that_exist(address_book):
+    """
+           to test edit contact with contact that exist
+           :param - address_book:
+           :return: None
+    """
     unique_detail = "9740142826"
     contact_to_update = Contact("agam", "singh", "cantt", "varanasi", "uttar pradesh", "221002", "9740142826",
                                 "xyz@gmail.com")
@@ -25,18 +41,33 @@ def test_edit_contact_that_exist(address_book):
 
 
 def test_delete_contact_with_correct_count(address_book):
+    """
+      to test delete contact with contact that exist
+      :param - address_book:
+      :return: None
+    """
     contact_to_delete = "agam"
     address_book.delete_contact(contact_to_delete)
     assert len(address_book.address_book) == 0
 
 
 def test_search_person_by_city_with_correct_city(address_book):
+    """
+         to test search person by city with city that exist
+         :param - address_book:
+         :return: None
+    """
     city_to_be_searched = "varanasi"
     result = address_book.search_person_by_city(city_to_be_searched)
     assert result == True
 
 
 def test_search_person_by_state_with_correct_state(address_book):
+    """
+        to test search person by state with state that exist
+        :param - address_book:
+        :return: None
+    """
     state_to_be_searched = "uttar pradesh"
     result = address_book.search_person_by_state(state_to_be_searched)
     assert result == True
@@ -52,13 +83,53 @@ def test_map_city_with_person_with_existing_city(address_book):
     assert len(city_map["varanasi"]) == 1
 
 
-@pytest.mark.xfail
-def test_add_contact_return_incorrect_count(address_book):
-    assert len(address_book.address_book) == 0
+@pytest.mark.xfail(raises=AddressBookException)
+def test_add_contact_given_empty_first_name():
+    """
+    to test add contact with empty first name
+    :return:
+    """
+    contact1 = Contact("", "singh", "cantt", "varanasi", "uttar pradesh", "221002", "123456789",
+                       "abc@gmail.com")
+
+
+@pytest.mark.xfail(raises=AddressBookException)
+def test_add_contact_given_none_first_name():
+    """
+    to test add contact with invalid first name
+    :return:
+    """
+    contact1 = Contact(None, "singh", "cantt", "varanasi", "uttar pradesh", "221002", "123456789",
+                       "abc@gmail.com")
+
+
+@pytest.mark.xfail(raises=AddressBookException)
+def test_add_contact_given_city_as_empty():
+    """
+    to test add contact with empty first name
+    :return:
+    """
+    contact1 = Contact("akash", "singh", "cantt", "", "uttar pradesh", "221002", "123456789",
+                       "abc@gmail.com")
+
+
+@pytest.mark.xfail(raises=AddressBookException)
+def test_add_contact_given_city_as_none():
+    """
+    to test add contact with invalid first name
+    :return:
+    """
+    contact1 = Contact("akash", "singh", "cantt", None, "uttar pradesh", "221002", "123456789",
+                       "abc@gmail.com")
 
 
 @pytest.mark.xfail
 def test_edit_contact_that_doesnot_exist(address_book):
+    """
+        to test edit contact with contact that  does not exist
+        :param - address_book:
+        :return: None
+    """
     unique_detail = "123456789"
     contact_to_update = Contact("agam", "singh", "cantt", "varanasi", "uttar pradesh", "221002", "123456789",
                                 "abc@gmail.com")
@@ -67,7 +138,12 @@ def test_edit_contact_that_doesnot_exist(address_book):
 
 
 @pytest.mark.xfail
-def test_delete_contact_with_incorrect_count(address_book):
+def test_delete_contact_with_contact_not_exist(address_book):
+    """
+        to test delete contact with contact that exist
+        :param - address_book:
+        :return: None
+    """
     contact_to_delete = "adam"
     address_book.delete_contact(contact_to_delete)
     assert len(address_book.address_book) == 0
@@ -75,6 +151,11 @@ def test_delete_contact_with_incorrect_count(address_book):
 
 @pytest.mark.xfail
 def test_search_person_by_city_with_incorrect_city(address_book):
+    """
+       to test search person by city with city  that does not exist
+       :param - address_book:
+       :return: None
+    """
     city_to_be_searched = "chennai"
     result = address_book.search_person_by_city(city_to_be_searched)
     assert result == True
@@ -82,6 +163,11 @@ def test_search_person_by_city_with_incorrect_city(address_book):
 
 @pytest.mark.xfail
 def test_search_person_by_state_with_incorrect_state(address_book):
+    """
+      to test search person by state with state  that does not exist
+      :param - address_book:
+      :return: None
+     """
     state_to_be_searched = "karnataka"
     result = address_book.search_person_by_state(state_to_be_searched)
     assert result == True
